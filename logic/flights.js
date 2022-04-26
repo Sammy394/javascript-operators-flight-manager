7fd360873f702e27c73f86e39cf69b2ef37abc87
Sammy394 <sam.h.strother@gmail.com>
2022-04-26 10:54:34 +1000

    Update flights.js

--- logic/flights.js
+++ logic/flights.js
@@ -1,18 +1,16 @@
 function Flights() {
+function calculateNumberOfFlights(passengers, capacity) {
+    let flights;
 
-
-function calculateNumberOfFlighs(passengers, capactiy) {
-  let flights;
-
-  if(passengers % capactiy == 0) {
-    flights = passengers/capactiy;
-  } else {
-    flights = Math.floor(passengers/capactiy) + 1;
-  }
-  return flights;
+    if(passengers % capacity == 0) {
+        flights = passengers/capacity;
+    } else {
+        flights =  Math.floor(passengers/capacity) + 1;
+    }
+    return flights;
 }
 
-return {calculateNumberOfFlighs};
+return {calculateNumberOfFlights};
 
 }
 
